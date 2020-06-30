e56b0f3093f454081de61aab67524c88ef68b688
Mason Stevens <mason.c.stevens@gmail.com>
2020-06-30 17:43:52 -0400

    Fragment wired up as blank screen

---
+++ app/src/main/java/galacticgadgets/galactic/android/bignerdranchreststudy/PhotoGalleryFragment.kt
@@ -0,0 +1,32 @@
+package galacticgadgets.galactic.android.bignerdranchreststudy
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.GridLayout
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class PhotoGalleryFragment : Fragment() {
+
+    private lateinit var photoRecyclerView: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
+
+        photoRecyclerView = view.findViewById(R.id.photo_recycler_view)
+        photoRecyclerView.layoutManager = GridLayoutManager(context, 3)
+
+        return view
+    }
+
+    companion object{
+        fun newInstance() = PhotoGalleryFragment()
+    }
+}
